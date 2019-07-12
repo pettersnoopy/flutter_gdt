21fc213c44f5e7b2fc3450aa7f07912263d6c8a8
luopeng <15158074877@163.com>
2019-07-12 11:06:40 +0800

    fix cache

--- android/src/main/java/com/example/flutter_gdt/managers/NativeExpressManager.java
+++ android/src/main/java/com/example/flutter_gdt/managers/NativeExpressManager.java
@@ -16,145 +16,154 @@ import io.flutter.plugin.common.MethodChannel.Result;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
- * @author luopeng
- * Created at 2019/7/9 10:45
+ * @author luopeng Created at 2019/7/9 10:45
  */
 public class NativeExpressManager {
-    public interface NativeExpressViewGetCallback {
-        void viewGet(NativeExpressADView view);
-        void viewGetError(int code, String reason);
-    }
-    private volatile static NativeExpressManager mInstance;
-    public static NativeExpressManager getInstance() {
+  public interface NativeExpressViewGetCallback {
+    void viewGet(NativeExpressADView view);
+
+    void viewGetError(int code, String reason);
+  }
+
+  private volatile static NativeExpressManager mInstance;
+
+  public static NativeExpressManager getInstance() {
+    if (mInstance == null) {
+      synchronized (NativeExpressManager.class) {
         if (mInstance == null) {
-            synchronized (NativeExpressManager.class) {
-                if (mInstance == null) {
-                    mInstance = new NativeExpressManager();
-                }
-            }
+          mInstance = new NativeExpressManager();
         }
-        return mInstance;
+      }
     }
+    return mInstance;
+  }
 
-    public NativeExpressManager() {
-        mNativeExpressAdViewCache = new HashMap<>();
-        mNativeExpressAdCache = new HashMap<>();
-    }
+  public NativeExpressManager() {
+    mNativeExpressAdViewCache = new HashMap<>();
+    mNativeExpressAdCache = new HashMap<String, NativeExpressAD>();
+  }
 
-    private HashMap<String, NativeExpressAD> mNativeExpressAdCache;
-    private HashMap<String, ArrayList<NativeExpressADView>> mNativeExpressAdViewCache;
-    private MethodChannel.Result mTmpResult;
+  private HashMap<String, NativeExpressAD> mNativeExpressAdCache;
+  private HashMap<String, ConcurrentLinkedQueue<NativeExpressADView>> mNativeExpressAdViewCache;
+  private MethodChannel.Result mTmpResult;
 
-    public void preloadNativeExpressAd(Activity activity, String appId, String positionId, ADSize adSize, int preloadCount) {
-        this.loadNativeExpressAd(activity, appId, positionId, adSize, preloadCount, null);
-    }
+  public void preloadNativeExpressAd(Activity activity, String appId, final String positionId, ADSize adSize,
+      int preloadCount) {
+    if (mNativeExpressAdCache.get(positionId) == null) {
+      mNativeExpressAdCache.put(positionId, new NativeExpressAD(activity, adSize, appId, positionId, new AdListener() {
+        @Override
+        public void onADLoaded(List<NativeExpressADView> list) {
+          putAdViewCache(positionId, list);
+        }
 
-    public void getNativeExpressView(
-            Activity activity,
-            String appId,
-            final String positionId,
-            ADSize adSize,
-            int preloadCount,
-            MethodChannel.Result result,
-            NativeExpressViewGetCallback callback) {
-        mTmpResult = result;
-
-        ArrayList<NativeExpressADView> adViews = mNativeExpressAdViewCache.get(positionId);
-        if (adViews != null && adViews.size() > 0) {
-            if (callback != null) {
-                callback.viewGet(getAdView(positionId));
-            }
-
-            if (adViews.size() <= 0 && preloadCount > 0) {
-                loadNativeExpressAd(activity, appId, positionId, adSize, preloadCount, null);
-            }
+        @Override
+        public void onRenderSuccess(NativeExpressADView nativeExpressADView) {
+          if (mTmpResult == null) {
             return;
+          }
+          mTmpResult.success(true);
+          mTmpResult = null;
         }
-
-        loadNativeExpressAd(activity, appId, positionId, adSize, preloadCount, callback);
+      }));
     }
 
-    private void loadNativeExpressAd(Activity activity, String appId, final String positionId, ADSize adSize, int loadCount, final NativeExpressViewGetCallback callback) {
-        if (mNativeExpressAdCache.get(positionId) == null) {
-            mNativeExpressAdCache.put(positionId, new NativeExpressAD(activity, adSize, appId, positionId, new NativeExpressAD.NativeExpressADListener() {
-                @Override
-                public void onADLoaded(List<NativeExpressADView> list) {
-                    putAdViewCache(positionId, list);
+    NativeExpressAD nativeExpressAD = mNativeExpressAdCache.get(positionId);
+    nativeExpressAD.loadAD(preloadCount);
+  }
 
-                    if (callback != null) {
-                        callback.viewGet(getAdView(positionId));
-                    }
-                }
+  public void getNativeExpressView(Activity activity, String appId, final String positionId, ADSize adSize,
+      int preloadCount, final MethodChannel.Result result, final NativeExpressViewGetCallback callback) {
+      mTmpResult = result;
+    ConcurrentLinkedQueue<NativeExpressADView> adViews = getViewQueue(positionId);
+    if (adViews != null && adViews.size() > 0) {
+      if (callback != null) {
+        callback.viewGet(getAdView(positionId));
+      }
 
-                @Override
-                public void onRenderFail(NativeExpressADView nativeExpressADView) {
-
-                }
-
-                @Override
-                public void onRenderSuccess(NativeExpressADView nativeExpressADView) {
-                    if (mTmpResult != null) {
-                        mTmpResult.success(true);
-                        mTmpResult = null;
-                    }
-                }
-
-                @Override
-                public void onADExposure(NativeExpressADView nativeExpressADView) {
-                }
-
-                @Override
-                public void onADClicked(NativeExpressADView nativeExpressADView) {
-
-                }
-
-                @Override
-                public void onADClosed(NativeExpressADView nativeExpressADView) {
-
-                }
-
-                @Override
-                public void onADLeftApplication(NativeExpressADView nativeExpressADView) {
-
-                }
-
-                @Override
-                public void onADOpenOverlay(NativeExpressADView nativeExpressADView) {
-
-                }
-
-                @Override
-                public void onADCloseOverlay(NativeExpressADView nativeExpressADView) {
-
-                }
-
-                @Override
-                public void onNoAD(AdError adError) {
-                    Log.e(Consts.TAG, "no ad error " + adError.getErrorCode() + "," + adError.getErrorMsg());
-                }
-            }));
-        }
-
-        NativeExpressAD nativeExpressAD = mNativeExpressAdCache.get(positionId);
-        nativeExpressAD.loadAD(loadCount);
+      if (adViews.isEmpty() && preloadCount > 0) {
+        preloadNativeExpressAd(activity, appId, positionId, adSize, preloadCount);
+      }
+      return;
     }
 
-    private void putAdViewCache(String posId, List<NativeExpressADView> list) {
-        synchronized (this) {
-            if (mNativeExpressAdViewCache.get(posId) == null) {
-                mNativeExpressAdViewCache.put(posId, new ArrayList<NativeExpressADView>());
-            }
-
-            mNativeExpressAdViewCache.get(posId).addAll(list);
+    new NativeExpressAD(activity, adSize, appId, positionId, new AdListener() {
+      @Override
+      public void onADLoaded(List<NativeExpressADView> list) {
+        if (list == null || list.isEmpty()) {
+          return;
         }
+        if (callback == null) {
+          return;
+        }
+        callback.viewGet(list.get(0));
+      }
+
+      @Override
+      public void onRenderSuccess(NativeExpressADView nativeExpressADView) {
+        if (mTmpResult == null) {
+          return;
+        }
+        mTmpResult.success(true);
+        mTmpResult = null;
+      }
+    }).loadAD(preloadCount);
+
+    preloadNativeExpressAd(activity, appId, positionId, adSize, preloadCount);
+  }
+
+  private synchronized ConcurrentLinkedQueue<NativeExpressADView> getViewQueue(String posId) {
+    if (mNativeExpressAdViewCache.get(posId) == null) {
+      mNativeExpressAdViewCache.put(posId, new ConcurrentLinkedQueue<NativeExpressADView>());
+    }
+    return mNativeExpressAdViewCache.get(posId);
+  }
+
+  private void putAdViewCache(String posId, List<NativeExpressADView> list) {
+    ConcurrentLinkedQueue<NativeExpressADView> queue = getViewQueue(posId);
+    queue.addAll(list);
+  }
+
+  private NativeExpressADView getAdView(String posId) {
+    ConcurrentLinkedQueue<NativeExpressADView> queue = getViewQueue(posId);
+    return (queue == null || queue.isEmpty()) ? null : queue.poll();
+  }
+
+  private abstract class AdListener implements NativeExpressAD.NativeExpressADListener {
+    @Override
+    public void onRenderFail(NativeExpressADView nativeExpressADView) {
     }
 
-    private NativeExpressADView getAdView(String posId) {
-        synchronized (this) {
-            ArrayList<NativeExpressADView> adViews = mNativeExpressAdViewCache.get(posId);
-            return (adViews == null || adViews.size() <= 0) ? null : adViews.remove(0);
-        }
+    @Override
+    public void onADExposure(NativeExpressADView nativeExpressADView) {
     }
+
+    @Override
+    public void onADClicked(NativeExpressADView nativeExpressADView) {
+    }
+
+    @Override
+    public void onADClosed(NativeExpressADView nativeExpressADView) {
+    }
+
+    @Override
+    public void onADLeftApplication(NativeExpressADView nativeExpressADView) {
+    }
+
+    @Override
+    public void onADOpenOverlay(NativeExpressADView nativeExpressADView) {
+    }
+
+    @Override
+    public void onADCloseOverlay(NativeExpressADView nativeExpressADView) {
+    }
+
+    @Override
+    public void onNoAD(AdError adError) {
+      Log.e(Consts.TAG, "No Ad Error " + adError.getErrorCode() + "," + adError.getErrorMsg());
+    }
+  }
 }
