71af333d1509c56f1008c5a1550becb027ba8ea8
luopeng <15158074877@163.com>
2019-09-01 15:52:59 +0800

    remove android gdt

--- android/src/main/java/com/example/flutter_gdt/FlutterGdtPlugin.java
+++ android/src/main/java/com/example/flutter_gdt/FlutterGdtPlugin.java
@@ -1,15 +1,7 @@
 package com.example.flutter_gdt;
-
-import android.Manifest;
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Build;
-
-import com.example.flutter_gdt.managers.NativeExpressManager;
-import com.example.flutter_gdt.views.FlutterNativeExpressViewFactory;
-import com.example.flutter_gdt.views.FlutterSplashAdViewFactory;
-import com.qq.e.ads.nativ.ADSize;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +11,6 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
-import io.flutter.plugin.common.StandardMessageCodec;
 
 /** FlutterGdtPlugin */
 public class FlutterGdtPlugin implements MethodCallHandler {
@@ -34,93 +25,11 @@ public class FlutterGdtPlugin implements MethodCallHandler {
     public static void registerWith(Registrar registrar) {
         final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_gdt");
         channel.setMethodCallHandler(new FlutterGdtPlugin(registrar.activity()));
-        registrar.platformViewRegistry().registerViewFactory("flutter_gdt_native_express_ad_view",
-                new FlutterNativeExpressViewFactory(new StandardMessageCodec(), registrar.activity(), registrar.messenger()));
-        registrar.platformViewRegistry().registerViewFactory("flutter_gdt_splash_ad_view",
-                new FlutterSplashAdViewFactory(new StandardMessageCodec(), registrar.activity(), registrar.messenger()));
     }
 
     @Override
     public void onMethodCall(MethodCall call, Result result) {
-        if ("checkPermissions".equals(call.method)) {
-            this.checkPermission(call, result);
-        } else if ("preloadNativeExpress".equals(call.method)) {
-            this.preloadNativeExpress(call, result);
-        }
+        result.notImplemented();
     }
 
-    private void checkPermission(MethodCall call, Result result) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            this.checkAndRequestPermission(result);
-        } else {
-            result.success(true);
-        }
-    }
-
-    private ArrayList<String> getNeedPermissionList() {
-        ArrayList<String> lackedPermission = new ArrayList<String>();
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if ((mActivity.checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED)) {
-                lackedPermission.add(Manifest.permission.READ_PHONE_STATE);
-            }
-
-            if ((mActivity.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
-                lackedPermission.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-            }
-
-            if ((mActivity.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
-                lackedPermission.add(Manifest.permission.ACCESS_FINE_LOCATION);
-            }
-        }
-        return lackedPermission;
-    }
-
-    @TargetApi(Build.VERSION_CODES.M)
-    private void checkAndRequestPermission(Result result) {
-        List<String> lackedPermission = getNeedPermissionList();
-
-        // 权限都已经有了，那么直接调用SDK
-        if (lackedPermission.size() == 0) {
-            result.success(true);
-        } else {
-            // 请求所缺少的权限，在onRequestPermissionsResult中再看是否获得权限，如果获得权限就可以调用SDK，否则不要调用SDK。
-            result.success(false);
-            String[] requestPermissions = new String[lackedPermission.size()];
-            lackedPermission.toArray(requestPermissions);
-            mActivity.requestPermissions(requestPermissions, 1024);
-        }
-    }
-
-    private void preloadNativeExpress(MethodCall call, Result result) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            List<String> lackedPermission = getNeedPermissionList();
-            if (lackedPermission.size() > 0) {
-                LogUtils.e(Consts.TAG, "no permission");
-                return;
-            }
-        }
-
-
-        String appId = call.argument("appId");
-        String posId = call.argument("positionId");
-        Object width = call.argument("width");
-        if (width == null) {
-            LogUtils.e(Consts.TAG, "no ad width");
-            return;
-        }
-
-        Object height = call.argument("height");
-        if (height == null) {
-            LogUtils.e(Consts.TAG, "no ad height");
-            return;
-        }
-
-        int preloadCount = Consts.DEFAULT_PRELOAD_COUNT;
-        Object preload = call.argument("preloadCount");
-        if (preload != null) {
-            preloadCount = (int) preload;
-        }
-
-        NativeExpressManager.getInstance().preloadNativeExpressAd(mActivity, appId, posId, new ADSize((int) width, (int) height), preloadCount);
-    }
 }
